4ca9ded7fb08c3fd75f75a001f663a905f09e627
Gorynych <goryn981044@gmail.com>
2024-11-19 02:25:16 +0300

    fix

--- src/bookmaker/Main.java
+++ src/bookmaker/Main.java
@@ -5,12 +5,8 @@ public class Main {
         Event footballMatch = new Event("Football Match", 2.0);
         Bet bet1 = new Bet("Alice", 100, footballMatch.getEventName());
         Bet bet2 = new Bet("Bob", 50, footballMatch.getEventName());
-
-        // Пример выигрыша
         bet1.setWin(true);
         bet2.setWin(false);
-
-        // Печать результата
         System.out.println("User " + bet1.getUser() + " won " + bet1.calculatePayout(footballMatch.getOdds()));
         System.out.println("User " + bet2.getUser() + " won " + bet2.calculatePayout(footballMatch.getOdds()));
     }
